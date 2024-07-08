68ad2f194fd4763f636f7b075504872773c0397b
Gesine Fengler <110592817+Gesine93@users.noreply.github.com>
2024-07-08 20:14:20 +0200

    added . button

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ addEventListener("DOMContentLoaded", (event) => {
     let display = document.querySelector("#display");
     let displayText = "";
     let numbers = ["1","2","3","4","5","6","7","8","9","0"];
-    let operators = ["+", "-", "×", "÷"];
+    let operators = ["+", "−", "×", "÷"];
     let operatorClicked = false;
     let equalClicked = false;
     let result;
@@ -30,6 +30,14 @@ addEventListener("DOMContentLoaded", (event) => {
                     displayText += button.textContent;
                     display.textContent = displayText;
                 }
+            } else if (operatorClicked && button.textContent === "." && !secondNum.includes(".")) {
+                secondNum += button.textContent;
+                displayText += button.textContent;
+                display.textContent = displayText;
+            } else if (!equalClicked && button.textContent === "." && !firstNum.includes(".")) {
+                firstNum += button.textContent;
+                displayText += button.textContent;
+                display.textContent = displayText;
             } else if (button.textContent === "=") {
                 equalClicked = true;
                 operatorClicked = false;
@@ -58,26 +66,31 @@ addEventListener("DOMContentLoaded", (event) => {
 })
 
 function add(a,b) {
-    return Number(a) + Number(b);
+    num = Number(a) + Number(b);
+    return roundToThree(num);
 }
 
 function subtract(a,b) {
-    return Number(a) - Number(b);
+    num = Number(a) - Number(b);
+    return roundToThree(num);
+
 }
 
 function multiply(a,b) {
-    return Number(a) * Number(b);
+    num = Number(a) * Number(b);
+    return roundToThree(num);
 }
 
 function divide(a,b) {
-    return Number(a) / Number(b);
+    num = Number(a) / Number(b);
+    return roundToThree(num);
 }
 
 function operate(firstNum, secondNum, operator) {
     let result;
     if (operator === "+") {
         result = add(firstNum, secondNum);
-    } else if (operator === "-") {
+    } else if (operator === "−") {
         result = subtract(firstNum, secondNum);
     } else if (operator === "×") {
         result = multiply(firstNum, secondNum);
@@ -85,4 +98,8 @@ function operate(firstNum, secondNum, operator) {
         result = divide(firstNum, secondNum);
     }
     return result;
+}
+
+function roundToThree(num) {
+    return +(Math.round(num + "e+3")  + "e-3");
 }
